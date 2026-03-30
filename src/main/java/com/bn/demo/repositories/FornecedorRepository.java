8968cb321526277ab454d6107728bb5f0b4be378
renanhomiak <renanhomiakg@gmail.com>
2026-03-30 20:01:11 -0300

    gerenciamento de fornecedores

--- src/main/java/com/bn/demo/repositories/FornecedorRepository.java
+++ src/main/java/com/bn/demo/repositories/FornecedorRepository.java
@@ -4,4 +4,4 @@ import com.bn.demo.models.FornecedorModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FornecedorRepository extends JpaRepository<FornecedorModel, Long> {
-}
+}gi
